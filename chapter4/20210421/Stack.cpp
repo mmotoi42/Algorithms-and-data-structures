155397d3b9863ab3e6b7ccf4efda1c7de066146a
mmotoi42 <mmotoi@student.42tokyo.jp>
2021-04-21 23:26:27 +0900

    Stack.cpp o

---
+++ chapter4/20210421/Stack.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	stack<int> S;
+	int a, b, x;
+	string s;
+
+	while (cin >> s)
+	{
+		if (s[0] == '+')
+		{
+			a = S.top(); S.pop();
+			b = S.top(); S.pop();
+			S.push(b + a);
+		}
+		else if (s[0] == '-')
+		{
+			a = S.top(); S.pop();
+			b = S.top(); S.pop();
+			S.push(b - a);
+		}
+		else if (s[0] == '*')
+		{
+			a = S.top(); S.pop();
+			b = S.top(); S.pop();
+			S.push(b * a);
+		}
+		else
+		{
+			S.push(atoi(s.c_str()));
+		}
+	}
+	cout << S.top() << endl;
+	return (0);
+}
