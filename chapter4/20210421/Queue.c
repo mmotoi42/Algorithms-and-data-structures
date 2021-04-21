3baea2a3f41b3c0d2f69a18b769ded071e06d416
mmotoi42 <mmotoi@student.42tokyo.jp>
2021-04-21 21:19:25 +0900

    Queue x

---
+++ chapter4/20210421/Queue.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#define LEN 100005
+
+typedef struct pp
+{
+	char name[100];
+	int t;
+} P;
+
+P Q[LEN];
+int head, tail, n;
+
+void	enqueue(P x)
+{
+	Q[tail] = x;
+	tail = (tail + 1) % LEN;
+}
+
+P	dequeue()
+{
+	P x = Q[head];
+
+	head = (head + 1) % LEN;
+	return x;
+}
+
+int min(int a, int b) {
+	return (a < b ? a : b);
+}
+
+int main()
+{
+	int elaps = 0, c;
+	int i, q;
+	P u;
+
+	scanf("%d %d", &n, &q);
+	for (int i = 0; i < n; i++)
+		scanf("%s %d", Q[i].name, &Q[i].t);
+	head = 0;
+	tail = n;
+
+	while (head != tail)
+	{
+		u = dequeue();
+		c = min(q, u.t);
+		u.t -= c;
+		elaps += c;
+		if (u.t > 0) enqueue(u);
+		else
+		{
+			printf("%s %d\n", u.name, elaps);
+		}
+	}
+	return (0);
+}
