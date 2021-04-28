cf910e3f388408b4dfbd431d5b154e53e95f32a9
mmotoi42 <mmotoi@student.42tokyo.jp>
2021-04-28 22:45:52 +0900

    practice Iterator

---
+++ chapter5/20210428/Iterator.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void	print(vector<int> v)
+{
+	for(vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << *it;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int N = 4;
+
+	vector<int> v;
+
+	for(int i = 0; i < N; i++)
+	{
+		int x;
+		cin >> x;
+		 v.push_back(x);
+	}
+
+	print(v);
+
+	vector<int>::iterator it = v.begin();
+	*it = 3;
+	it++;
+	(*it)++;
+
+	print(v);
+	return (0);
+}
